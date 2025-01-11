0997169b33eee9743b304943f95fd92c7339e25f
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-01-11 19:35:57 +0100

    add About component and integrate it into App layout

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='min-h-screen w-screen'>
+        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:text-[10px]'>
+                Welcome to Zentry
+            </h2>
+            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+                Discover the world's largest shared adventure
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
