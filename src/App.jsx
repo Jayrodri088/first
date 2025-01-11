bd81cc6f8ae51facd23c57d332f1b29e557713cb
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-01-11 11:45:12 +0100

    initialize project with Vite, React, and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+        <h1 className='text-5xl text-violet-300 font-bold '>Welcome to awards</h1>
+    </main>
+  )
+}
+
+export default App
