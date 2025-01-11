b792e0f2af23d1ca90c2b0e27a604af94bd1b26a
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-01-11 19:30:34 +0100

    add Hero component and Button component; update App layout and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <main>
-        <h1 className='text-5xl text-violet-300 font-bold '>Welcome to awards</h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+        <Hero/>
+        <section className='z-0 min-h-screen bg-blue-500'/>
     </main>
   )
 }
