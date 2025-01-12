b89b291e9abd7de67c005e83aa34c58c33ca31d7
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-01-12 22:14:54 +0100

    add Story, Contact, and Footer components; integrate into App layout; add VideoPreview component for enhanced video interaction; update styles and animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,9 @@ import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import VideoPreview from "./VideoPreview";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -32,7 +33,7 @@ const Hero = () => {
     if (loadedVideos === totalVideos - 1) {
       setIsLoading(false);
     }
-  },[loadedVideos])
+  }, [loadedVideos]);
 
   useGSAP(
     () => {
@@ -52,10 +53,10 @@ const Hero = () => {
         });
 
         gsap.from("#current-video", {
-            transformOrigin: "center center",
-            scale: 0,
-            duration: 1.5,
-            ease: "power1.inOut",
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
         });
       }
     },
@@ -63,23 +64,23 @@ const Hero = () => {
   );
 
   useGSAP(() => {
-    gsap.set('#video-frame', {
-      clipPath: 'polygon(10% 0, 81% 0, 94% 95%, 1% 100%)',
-      borderRadius: '0 0 40% 10%'
-    })
+    gsap.set("#video-frame", {
+      clipPath: "polygon(10% 0, 81% 0, 94% 95%, 1% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
 
-    gsap.from('#video-frame', {
-      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-      borderRadius: '0 0 0 0',
-      ease: 'power2.inOut',
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power2.inOut",
       scrollTrigger: {
-        trigger: '#video-frame',
-        start: 'center center',
-        end: 'bottom center',
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
         scrub: true,
-      }
-    })
-  })
+      },
+    });
+  });
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -100,20 +101,22 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div
-              onClick={handleMiniVdClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-            >
-              <video
-                ref={nextVideoRef}
-                src={getVideoSrc(upcomingVideoIndex)}
-                loop
-                muted
-                id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoad}
-              />
-            </div>
+            <VideoPreview>
+              <div
+                onClick={handleMiniVdClick}
+                className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+              >
+                <video
+                  ref={nextVideoRef}
+                  src={getVideoSrc(upcomingVideoIndex)}
+                  loop
+                  muted
+                  id="current-video"
+                  className="size-64 origin-center scale-150 object-cover object-center"
+                  onLoadedData={handleVideoLoad}
+                />
+              </div>
+            </VideoPreview>
           </div>
           <video
             ref={nextVideoRef}
@@ -128,7 +131,7 @@ const Hero = () => {
             src={getVideoSrc(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
